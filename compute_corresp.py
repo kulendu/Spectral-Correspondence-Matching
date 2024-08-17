0097b3e0851314728534080db0f6b18e2ca32230
kulendu <kulendu>
2024-08-18 00:13:29 +0530

    Update: Added sigma for plotting affinities at different sclaes

--- compute_corresp.py
+++ compute_corresp.py
@@ -2,6 +2,7 @@ import numpy as np
 import pickle
 import polyscope as ps
 import matplotlib.pyplot as plt
+import scipy.linalg
 from smpl import SMPL
 import torch
 import tensorflow as tf
@@ -12,6 +13,7 @@ from scipy.linalg import eigh
 import vedo
 import trimesh
 import networkx as nx 
+from tqdm import tqdm
 
 
 SMPL_PATH = './SMPL_NEUTRAL.pkl'
@@ -80,7 +82,7 @@ def skel_visualizer():
     edges_smpl = np.array(EDGES_SMPL)
     edges_rabit = np.array(EDGES_RABIT)
     
-    breakpoint()
+    # breakpoint()
     smpl_joints = ps.register_point_cloud("SMPL joints", joints)
     skel = ps.register_curve_network("Connections", joints, edges_smpl)
     r_joints = ps.register_point_cloud("Rabit Joints", rabit_joints)
@@ -148,7 +150,7 @@ def laplacian():
     # skel_visualizer()
     # render_eign(eigenvectors_smpl, eigenvalues_smpl)
 
-    return eigenvalues_smpl, eigenvectors_smpl, eigenvalues_rabit, eigenvectors_rabit, laplacian_dense, laplacian_dense_r
+    return eigenvalues_smpl, eigenvectors_smpl, eigenvalues_rabit, eigenvectors_rabit, laplacian_dense, laplacian_dense_r, adj_sparse
 
     # plt.figure(figsize=(10, 4))
     # plt.subplot(1, 2, 1)
@@ -196,7 +198,7 @@ def compute_hks(evals, evecs, count):
     evecs = torch.from_numpy(evecs).type(torch.float32)
     print(f"evals: {type(evals)} \n evecs = {type(evecs)}")
     
-    scales = torch.logspace(-2, 0., steps=count, dtype=torch.float32)
+    scales = torch.logspace(-2, 0., steps=count, dtype=torch.float32) # scales (timestep) = 16
     print(f"Shape of scales = {scales.shape}")
 
     if len(evals.shape) == 1:
@@ -209,8 +211,8 @@ def compute_hks(evals, evecs, count):
 
     print(f"evals: {type(evals)} \n evecs = {type(evecs)} \n scales: {type(scales)}")
     print(f"Scales: {scales}")
-    power_coefs = torch.exp(-evals.unsqueeze(1) * scales.unsqueeze(-1)).unsqueeze(1)
-    terms = power_coefs * (evecs * evecs).unsqueeze(2)
+    power_coefs = torch.exp(-evals.unsqueeze(1) * scales.unsqueeze(-1)).unsqueeze(1) # shape: [1, 1, 16, 24]
+    terms = power_coefs * (evecs * evecs).unsqueeze(2) # shape: [1, 24, 16, 24]
     
     out = torch.sum(terms, dim=-1) 
 
@@ -219,8 +221,8 @@ def compute_hks(evals, evecs, count):
 
 def visualize_corresp(k, f_dim, eval_smpl, evec_smpl, eval_rabit, evec_rabit, j_smpl, j_rabit):
 
-    hks_smpl = compute_hks(eval_smpl[:k], evec_smpl[:,:k], f_dim) #(B,V,feature_dim)
-    hks_rabit = compute_hks(eval_rabit[:k], evec_rabit[:,:k], f_dim) #(B,V,feature_dim)
+    hks_smpl = compute_hks(eval_smpl[:k], evec_smpl[:,:k], f_dim) # (B,V,feature_dim)
+    hks_rabit = compute_hks(eval_rabit[:k], evec_rabit[:,:k], f_dim) # (B,V,feature_dim)
     
     color_smpl = hks_smpl[0].numpy()[:,:3]
     color_smpl = (color_smpl - color_smpl.min())/(color_smpl.max()-color_smpl.min())
@@ -243,7 +245,7 @@ def visualize_corresp(k, f_dim, eval_smpl, evec_smpl, eval_rabit, evec_rabit, j_
     ps.show()
 
 # feature-wise geodesic distance
-def compute_mat(k, f_dim, eval_smpl, evec_smpl, eval_rabit, evec_rabit, joints, joints_rabit):
+def compute_mat(k, f_dim, eval_smpl, evec_smpl, eval_rabit, evec_rabit, joints, joints_rabit, adj_sparse):
     hks_smpl = compute_hks(eval_smpl[:k], evec_smpl[:,:k], f_dim) #(B,V,feature_dim)
     hks_rabit = compute_hks(eval_rabit[:k], evec_rabit[:,:k], f_dim) #(B,V,feature_dim)
 
@@ -298,58 +300,192 @@ def compute_mat(k, f_dim, eval_smpl, evec_smpl, eval_rabit, evec_rabit, joints, 
     # plt.show()
 
     num_joints = 24
-    corresp_mat = torch.zeros((num_joints**2, num_joints**2))
-    norm_corresp_mat = torch.zeros((num_joints**2, num_joints**2))
-
+    
+    pair_nodes = torch.zeros((num_joints**2,f_dim))
     for i in range(num_joints):
         for j in range(num_joints):
-            for k in range(num_joints):
-                for l in range(num_joints):
-                    node1 = ((hks_smpl[0][i] - hks_rabit[0][j])**2).mean()
-                    node2 = ((hks_smpl[0][k] - hks_rabit[0][l])**2).mean()
-                    corresp_mat[(i*num_joints)+j, (k*num_joints)+l] = ((node1-node2)**2).mean()
-                    # corresp_mat[(i*num_joints)+j, (k*num_joints)+l] = np.mean((smpl_mat[i][j] - rabit_mat[k][l])**2)
-    
-    
-    for i in range(num_joints**2):
-        norm_corresp_mat[i] = (corresp_mat[i] - corresp_mat[i].min()) / (corresp_mat[i].max() - corresp_mat[i].min())
-    
-    corresp_mat = corresp_mat.numpy()
-    norm_corresp_mat = norm_corresp_mat.numpy()
-    # norm_mat_corresp = (corresp_mat - corresp_mat.min()) / (corresp_mat.max() - corresp_mat.min())
-    print(f"\n Correspondence Matrix: {norm_corresp_mat} and Shape: {norm_corresp_mat.shape}")
-    
-    # plt.imshow(norm_corresp_mat)
-    # plt.title("Correspondence Matrix")
-    # plt.colorbar()
-    # plt.axis([0, num_joints**2 -1, 0, num_joints**2-1]) 
-    # # plt.gca().invert_xaxis()
-    # plt.show()
+            pair_nodes[i*num_joints + j] = (hks_smpl[0][i] - hks_rabit[0][j])**2
+    pair_nodes = pair_nodes.numpy()
+
+    sigma_values = np.arange(0.3, 0.5, 0.001)
+
+    for sigma in sigma_values:
+        corresp_mat = torch.zeros((num_joints**2, num_joints**2))
+        norm_corresp_mat = torch.zeros((num_joints**2, num_joints**2))
+
+        for i in range(num_joints):
+            for j in range(num_joints):
+                for k in range(num_joints):
+                    for l in range(num_joints):
+                        node1 = ((hks_smpl[0][i] - hks_rabit[0][j])**2).mean()
+                        node2 = ((hks_smpl[0][k] - hks_rabit[0][l])**2).mean()
+                        
+
+
+                        # print(f"\n Sigma Values: {sigma_values} \n")
+                        corresp_mat[(i*num_joints)+j, (k*num_joints)+l] = np.exp(-((node1-node2)**2)/sigma).mean()
+                        # corresp_mat[(i*num_joints)+j, (k*num_joints)+l] = np.mean((smpl_mat[i][j] - rabit_mat[k][l])**2)
+        # corresp_mat = corresp_mat.numpy()
+        # np.fill_diagonal(corresp_mat, 0)
+        # A = nx.from_numpy_array(corresp_mat)
+
+        # affinities = []
+
+        # for node1, node2, attr in A.edges(data=True):
+        #     weight = attr['weight']
+        #     # attr['affinity'] = weight
+        #     affinities.append(weight)
+                
+
+        # bins = np.arange(0.0, 1.0, 0.1)
+        # plt.hist(affinities, color='skyblue', bins=bins, edgecolor='black')
+        # plt.xlabel('Affinity')
+        # plt.ylabel('Frequency')
+        # plt.show()
+                                    
+
 
     # breakpoint()
-    # GG = nx.from_numpy_array(corresp_mat)
-    # nx.draw(GG, with_labels=True, node_color='lightblue', edge_color='black')
-    # plt.show()
+    # for i in range(num_joints**2):
+    #     norm_corresp_mat[i] = (corresp_mat[i] - corresp_mat[i].min()) / (corresp_mat[i].max() - corresp_mat[i].min())
     
-    print("-------------------------- \n")
+        corresp_mat = corresp_mat.numpy()
+        norm_corresp_mat = norm_corresp_mat.numpy()
+        # norm_mat_corresp = (corresp_mat - corresp_mat.min()) / (corresp_mat.max() - corresp_mat.min())
+        print(f"\n Correspondence Matrix: {corresp_mat} and Shape: {corresp_mat.shape}")
+        
+        # plt.imshow(norm_corresp_mat)
+        # plt.title("Correspondence Matrix")
+        # plt.colorbar()
+        # plt.axis([0, num_joints**2 -1, 0, num_joints**2-1]) 
+        # # plt.gca().invert_xaxis()
+        # plt.show()
+
+        # GG = nx.from_numpy_array(corresp_mat)
+        # nx.draw(GG, with_labels=True, node_color='lightblue', edge_color='black')
+        # plt.show()
+        
+        print("-------------------------- \n")
+        
+        # Creating the Association matrix
+        np.fill_diagonal(corresp_mat, 0) # diagonal entries = 0
+        print(f"\n Corresp matrix after removing the diaginal: {corresp_mat}")
+        A = nx.from_numpy_array(corresp_mat)
+
+        thresholds = [0.0, 0.900, 0.998, 1.0]
+        threshold_ranges = []
+        affinities = []
+
+        colors = ['blue', 'green', 'red']
+        
+
+
+        # for i in range(len(thresholds)):
+        #     threshold_ranges.append((thresholds[i], thresholds[i + 1]))
+
+        for node1, node2, attr in A.edges(data=True):
+            weight = attr['weight']
+            # attr['affinity'] = weight
+            affinities.append(weight)
+
+        pos = nx.spring_layout(A, seed=7)
+        # nx.draw_networkx_nodes(A, pos, node_size=400)
+
+        edges = A.edges(data=True)
+        
+        edge_colors = []
+        
+
+        # subgraphs = []
+
+        # for i in range(len(threshold_ranges)):
+        #     low, high = threshold_ranges[i]
+        #     subgraph = nx.Graph()
+        #     subgraph.add_nodes_from(A.nodes())
+            
+        #     for u, v, data in A.edges(data=True):
+        #         weight = data['weight']
+        #         if low < weight <= high:
+        #             subgraph.add_edge(u, v, weight=weight)
+            
+        #     subgraphs.append((subgraph, colors[i]))
+
+
+        for node1, node2, data in edges:
+            if data['weight'] == 1.0:
+                edge_colors.append('blue')
+            else:
+                edge_colors.append('white')
+
+
+        # fig, axes = plt.subplots(1, 3, figsize=(20, 6))
+        # for index, (subgraph, color) in enumerate(subgraphs):
+        #     ax = axes[index]
+        #     pos = nx.spring_layout(subgraph)
+        #     nx.draw(subgraph, pos, with_labels=True, node_color='lightgray', edge_color=color, node_size=500, font_size=16, font_color='black', edgecolors='black', ax=ax)
+        #     low, high = threshold_ranges[index]
+        #     ax.set_title(f'Edges in Range ({low} - {high})')
+
+        # plt.tight_layout()
+        # plt.show()
+
+        bins = np.linspace(0.0, 1.0, num=10)
+        # breakpoint()
+        plt.hist(affinities, color='skyblue', bins=bins, edgecolor='black')
+        plt.ylim(0, len(affinities))
+        plt.xlabel('Affinity')
+        plt.ylabel('Frequency')
+        plt.savefig(f"{sigma}.png")
+        print(f"------ Saving figures for {sigma} --------")
+        plt.clf()
+
+        # nx.draw_networkx_edges(A, pos, edge_color=edge_colors, width=2)
+        # nx.draw_networkx_labels(A, pos, font_size=10, font_color="black")
+        # plt.figure(figsize=(8, 6))
+        # nx.draw(A, pos, with_labels=True, edge_color=edge_colors, node_size=400, font_size=10, font_color='white', edgecolors=edge_colors)
+        # plt.title("Graph Visualization with Affinity-Based Edge Coloring")
+        # plt.show()
+
+
+
+
+
 
     '''Getting the Degree matrix'''
-    D = np.zeros_like(norm_corresp_mat)
+    D = np.zeros_like(corresp_mat)
 
-    for i in range(norm_corresp_mat.shape[0]):
-        row_sum = np.sum(norm_corresp_mat[i, :])
+    for i in range(corresp_mat.shape[0]):
+        row_sum = np.sum(corresp_mat[i, :])
         D[i, i] = row_sum
-    breakpoint()
-    print(f"Shape of the Diagonal Matrix: {D.shape}, \n and D: {D} ")
+    print(f"Shape of the Degree Matrix: {D.shape}, \n and D: {D} ")
 
-    L = norm_corresp_mat - D
-    breakpoint()
+    L = D - corresp_mat 
+    print(f"Shape of the Lapalcian Matrix: {L.shape}, \n and L: {L} ")
+
+    # A_nx = nx.from_numpy_array(adj_sparse)
+    # L_nx = nx.laplacian_matrix(A_nx).toarray()
+    eigenvalues, eigenvectors = scipy.linalg.eigh(L)
+    eigenvectors = eigenvectors.sort()
+    fiedler_vector = eigenvectors[1]
+    proj = corresp_mat @ fiedler_vector
+
+
+    # partition_1 = []
+    # partition_2 = []
+
+    # for i in range(len(A_nx.nodes)):
+    #     if fiedler_vector[i] >= 0:
+    #         partition_1.append(A_nx.nodes[i])
+    #     else:
+    #         partition_2.append(A_nx.nodes[i])
+
+
 
 
 if __name__ == '__main__':
     K=24 #number of eigenvectors to consider
     feature_dim = 16 # timesteps for the heat diffusion
-    eigenvalues_smpl, eigenvectors_smpl, eigenvalues_rabit, eigenvectors_rabit, laplacian_dense, laplacian_dense_r = laplacian()
+    eigenvalues_smpl, eigenvectors_smpl, eigenvalues_rabit, eigenvectors_rabit, laplacian_dense, laplacian_dense_r, adj_sparse = laplacian()
 
     joints = load_smpl(SMPL_PATH)
     rabit_joints = load_rabit(RABIT_PATH)
@@ -360,7 +496,7 @@ if __name__ == '__main__':
     # vis = visualize_corresp(K, feature_dim, eigenvalues_smpl, eigenvectors_smpl, 
     #                         eigenvalues_rabit, eigenvectors_rabit, joints, rabit_joints)
 
-    corresp_mat = compute_mat(K, feature_dim, eigenvalues_smpl, eigenvectors_smpl, eigenvalues_rabit, eigenvectors_rabit, joints, rabit_joints)
+    corresp_mat = compute_mat(K, feature_dim, eigenvalues_smpl, eigenvectors_smpl, eigenvalues_rabit, eigenvectors_rabit, joints, rabit_joints, adj_sparse)
 
     print("-------------------------- \n")
     # print(laplacian_dense)
